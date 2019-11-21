038d26dfcc16c38e59fb6d8048ce3a5c55dde926
franco sanchez <franco_genta@hotmail.com>
2019-11-21 16:30:46 -0300

    agregue procfile

--- src/config/conexionDB.js
+++ src/config/conexionDB.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 
 // conexion a la DB
-mongoose.connect(config.db, {useNewUrlParser: true})
-.then(() => console.log('Data Base Conect'))
-.catch((err) => console.log(`Data Base Error Conect: ${err}`))
+mongoose.connect(config.db, { useNewUrlParser: true })
+    .then(() => console.log('Data Base Conect'))
+    .catch((err) => console.log(`Data Base Error Conect: ${err}`))
